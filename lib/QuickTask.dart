404481eda6a9c21322d5c3376a9fcf539b1375c1
scribble-riyad <scribbleriyad@gmail.com>
2023-05-10 13:31:47 +0600

    quickTaskbox added

---
+++ lib/QuickTask.dart
@@ -0,0 +1,21 @@
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 1)
+class QuickTask {
+  QuickTask(
+      {this.id,
+      this.taskNo,
+      this.tasktitle,
+      this.taskdetails,
+      this.taskcreatedAt});
+  @HiveField(0)
+  int? id;
+  @HiveField(1)
+  int? taskNo;
+  @HiveField(2)
+  String? tasktitle;
+  @HiveField(3)
+  String? taskdetails;
+  @HiveField(4)
+  String? taskcreatedAt;
+}
